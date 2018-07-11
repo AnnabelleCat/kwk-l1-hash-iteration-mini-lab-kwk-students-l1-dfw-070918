20bc7e36cca03d78ce400354ea09f3e000e24fc6
AnnabelleCat <pikainka@gmail.com>
2018-07-11 19:21:23 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -52,7 +52,11 @@ def iterate_through_keys
   
   summer_olympics[:Atlanta] = "1996"
   
-  puts summer_olympics .keys
+  summer_olympics .keys .each do |cities| #makes array with only keys
+    upcased cities = []
+    upcased cities << cities.upcase
+  end
+  
   
 end
 iterate_through_keys
