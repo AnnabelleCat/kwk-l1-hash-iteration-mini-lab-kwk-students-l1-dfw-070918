dc9cbf4aaeecd4d239dd9389ac77e2ed317109a9
AnnabelleCat <pikainka@gmail.com>
2018-07-11 19:13:16 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -51,8 +51,7 @@ def iterate_through_keys
   }
   
   summer_olympics[:Atlanta] = "1996"
-  summer_olympics_cities = []
-  summer_olympics .each_key = summer_olympics_cities
+  puts summer_olympics.keys
   puts summer_olympics_cities
   
 end
