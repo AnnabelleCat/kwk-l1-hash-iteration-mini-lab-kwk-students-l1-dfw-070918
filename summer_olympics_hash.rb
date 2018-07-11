4439e4e0d9f632a06b9f50ebf283f465393867dc
AnnabelleCat <pikainka@gmail.com>
2018-07-11 19:06:15 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -20,12 +20,25 @@ def add_a_key_value_pair
   
   summer_olympics[:Atlanta] = "1996"
   summer_olympics
-  
 end
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  summer_olympics = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
+  
+  summer_olympics[:Atlanta] = "1996"
+  
+  summer_olympics.keys.each do |summer_olympics_keys|
+  summer_olympics.values.each do |summer_olympics_values|
+    puts "The #{summer_olympics_values} summer olympics took place in #{summer_olympics_keys}."
+  end
+end
 end
 
 def iterate_through_keys
